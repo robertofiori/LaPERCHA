3330c1e458684e6ecbd3a305034ddac843103ce5
robertofiori <167255902+robertofiori@users.noreply.github.com>
2024-12-06 21:07:56 -0300

    IMAGENES

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const productos = [
-    { id: 1, nombre: "Camiseta Básica", precio: 29.99, imagen: "https://via.placeholder.com/200", descripcion: "Camiseta de algodón, ideal para el día a día." },
-    { id: 2, nombre: "Pantalón Ajustado", precio: 49.99, imagen: "https://via.placeholder.com/200", descripcion: "Pantalón ajustado de mezclilla, cómodo y elegante." },
-    { id: 3, nombre: "Chaqueta de Cuero", precio: 99.99, imagen: "https://via.placeholder.com/200", descripcion: "Chaqueta de cuero sintético con acabados modernos." },
+    { id: 1, nombre: "Camiseta Básica", precio: 27105, imagen: "https://f.fcdn.app/imgs/c4c7cf/www.textilshop.com.uy/tex/9d5a/original/catalogo/50005000cor1/1920-1200/camiseta-basica-hombre-coral.jpg", descripcion: "Camiseta de algodón, ideal para el día a día." },
+    { id: 2, nombre: "Pantalón Ajustado", precio: 68599, imagen: "https://acdn.mitiendanube.com/stores/001/776/845/products/diseno-sin-titulo-201-e4e3ec6c3e11fe9d6416486643311049-1024-1024.jpg", descripcion: "Pantalón ajustado de mezclilla, cómodo y elegante." },
+    { id: 3, nombre: "Chaqueta de Cuero", precio: 290000 , imagen: "https://acdn.mitiendanube.com/stores/001/661/615/products/296cf98f-3874-4c32-96d5-5ec918a56f3f-c0b8f3d933aee0789217114906733621-1024-1024.jpg", descripcion: "Chaqueta de cuero sintético con acabados modernos." },
   ];
   
   const productosLista = document.getElementById("productos-lista");
